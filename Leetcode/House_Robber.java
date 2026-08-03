1bf63607ef5deef8e2d517bd906f366c0ce73ced
Pallavi Verma <vpallavi083@gmail.com>
2026-08-03 23:46:11 +0530

    Create House_Robber.java

---
+++ Leetcode/House_Robber.java
@@ -0,0 +1,26 @@
+/**
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+*/
+class Solution {
+    public int rob(int[] nums) {
+
+        int n = nums.length;
+
+        if (n == 1)
+            return nums[0];
+
+        int[] a = new int[n];
+
+        a[0] = nums[0];
+        a[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            a[i] = Math.max(a[i - 1], nums[i] + a[i - 2]);
+        }
+
+        return a[n - 1];
+    }
+}
