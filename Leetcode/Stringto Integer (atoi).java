2a8a66bef66a8148a6b6724c9cf2cf866def73e1
Pallavi Verma <vpallavi083@gmail.com>
2026-09-05 22:42:58 +0530

    Create Stringto Integer (atoi).java

---
+++ Leetcode/Stringto Integer (atoi).java
@@ -0,0 +1,46 @@
+/**
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+
+The algorithm for myAtoi(string s) is as follows:
+
+Whitespace: Ignore any leading whitespace (" ").
+Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+*/
+
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        int a = 1;
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                a = -1;
+            }
+            i++;
+        }
+
+        long res = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            res = res * 10 + (s.charAt(i) - '0');
+
+            if (a == 1 && res > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (a == -1 && -res < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int)(a * res);
+    }
+}
