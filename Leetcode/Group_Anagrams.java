8abe50fc39fd87ece701a59de9638ceaa393cae2
Pallavi Verma <vpallavi083@gmail.com>
2026-09-06 22:24:36 +0530

    Add groupAnagrams method to group anagrams
    Implemented a method to group anagrams from an array of strings using a HashMap.

---
+++ Leetcode/Group_Anagrams.java
@@ -0,0 +1,32 @@
+/**
+rray of strings strs, group the anagrams together. You can return the answer in any order.
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+*/
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
