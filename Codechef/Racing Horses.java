7b21c26e18004f170c4afd76b24dfab97e7fa169
Pallavi Verma <vpallavi083@gmail.com>
2026-09-26 21:53:01 +0530

    Add Racing Horses problem solution in Java
    Implement a program to find the minimum skill difference between two horses for multiple test cases.

---
+++ Codechef/Racing Horses.java
@@ -0,0 +1,45 @@
+/**
+Chef is very fond of horses. He enjoys watching them race. As expected, he has a stable full of horses. He, along with his friends, goes to his stable during the weekends to watch a few of these horses race. Chef wants his friends to enjoy the race and so he wants the race to be close. This can happen only if the horses are comparable on their skill i.e. the difference in their skills is less.
+
+There are N horses in the stable. The skill of the horse i is represented by an integer S[i]. The Chef needs to pick 2 horses for the race such that the difference in their skills is minimum. This way, he would be able to host a very interesting race. Your task is to help him do this and report the minimum difference that is possible between 2 horses in the race.
+
+Input:
+First line of the input file contains a single integer T, the number of test cases.
+Every test case starts with a line containing the integer N.
+The next line contains N space separated integers where the i-th integer is S[i].
+
+Output:
+For each test case, output a single line containing the minimum difference that is possible.
+*/
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+
+            long[] s = new long[n];
+
+            for (int i = 0; i < n; i++) {
+                s[i] = scanner.nextLong();
+            }
+
+            Arrays.sort(s);
+
+            long minDiff = Long.MAX_VALUE;
+
+            for (int i = 1; i < n; i++) {
+                minDiff = Math.min(minDiff, s[i] - s[i - 1]);
+            }
+
+            System.out.println(minDiff);
+        }
+
+        scanner.close();
+    }
+}
